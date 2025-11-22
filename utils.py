46f44ff7e112c829e338c3796aa0814c0fd957fd
Laurahrndz08 <103008572+Laurahrndz08@users.noreply.github.com>
2025-11-22 01:52:16 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+
+def load_image_from_bytes(image_bytes):
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
